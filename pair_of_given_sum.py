33accd7ae47cda5c2a59f2154c99f594fb9ab4f5
Jaki Sharief Shaik <shaikj@oregonstate.edu>
2018-11-01 21:37:50 -0700

    Finding a pair whose sum equals to x in an array

---
+++ pair_of_given_sum.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+#using Hash table
+
+def find_pair(arr,x):
+    ele_dict = defaultdict(int)
+    res = []
+    for ele in arr:
+        ele_dict[ele] = 1
+    for ele in arr:
+        if abs(x-ele) and ele_dict[abs(x-ele)] == 1:
+            res.append((ele,abs(x-ele)))
+            ele_dict[ele] = 0
+    return res
+
+print(find_pair([1,3,2,4,6,5],9))
